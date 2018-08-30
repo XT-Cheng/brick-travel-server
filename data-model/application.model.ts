cefeabb8183c4c119162edccaaa314e9dc2d38c2
Cheng <allen.cheng@te.com>
2018-08-30 12:54:41 +0800

    commit

--- data-model/application.model.ts
+++ data-model/application.model.ts
@@ -2,8 +2,41 @@ import {
   ModelType,
   prop,
   staticMethod,
-  Typegoose
+  Typegoose,
+  arrayProp
 } from "../typegoose/typegoose";
+
+export class ApplicationMenu extends Typegoose {
+  @prop()
+  _id: string;
+  @prop()
+  get id(): string {
+    return this._id;
+  }
+  set id(value) {
+    this._id = value;
+  }
+  @prop()
+  name: string;
+  @prop()
+  link: string;
+  @prop()
+  icon: string;
+  @prop()
+  acl: string;
+}
+
+new ApplicationMenu().getModelForClass(ApplicationMenu, {
+  schemaOptions: {
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret, options) => {
+        delete ret._id;
+        return ret;
+      }
+    }
+  }
+});
 
 export class Application extends Typegoose {
   @prop()
@@ -19,6 +52,8 @@ export class Application extends Typegoose {
   name: string;
   @prop()
   version: string;
+  @arrayProp({ items: ApplicationMenu })
+  menus: ApplicationMenu[];
   @staticMethod
   static findApplication(this: ModelType<Application> & typeof Application) {
     return this.find();
